0b752f1d1c461d395ec0d49fb5b837f9e82cbae1
victormusembi11 <victor.musembi11@gmail.com>
2023-10-24 22:17:11 +0300

    feat: added humburger menu and profile icon placeholder

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,7 +7,24 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ToDo App')),
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/profile.png')),
+          ),
+        ]),
+      ),
       body: Container(
         child: Text('Hello World!'),
       ),
