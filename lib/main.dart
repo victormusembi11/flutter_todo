4d2e68a0dc004e430c2e199c22e8b458b05cfea0
victormusembi11 <victor.musembi11@gmail.com>
2023-11-11 18:59:33 +0300

    feat: wrap app in GetMaterialApp to navigate pages w/ getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/my_home_page.dart';
 import 'package:flutter_todo/screens/home.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ToDo App',
       // home: Home(),
