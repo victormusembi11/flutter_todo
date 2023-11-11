c0f6013fd6d9364c41f4380ecedab8561e848f34
victormusembi11 <victor.musembi11@gmail.com>
2023-11-11 17:31:13 +0300

    feat: created tap buttons

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,10 +1,104 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SizedBox(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xFF89dad0),
+                ),
+                child: const Center(
+                    child: Text(
+                        "tap",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        )
+                    )
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFF89dad0),
+                ),
+                child: const Center(
+                    child: Text(
+                        "tap",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        )
+                    )
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFF89dad0),
+                ),
+                child: const Center(
+                    child: Text(
+                        "tap",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        )
+                    )
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFF89dad0),
+                ),
+                child: const Center(
+                    child: Text(
+                        "tap",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        )
+                    )
+                ),
+              ),
+            ),
+          ],
+        )
+      )
+    );
   }
 }
